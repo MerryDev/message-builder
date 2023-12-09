f19baa417ac9643d6d66463de3f509c7189c4434
Merry <svgr1808@outlook.de>
2023-12-09 16:25:00 +0100

    Add message builder service and related utilities
    This commit introduces a new MessageBuilder service using Java's ServiceLoader mechanism. A util class, Services, has been added to simplify service loading. Also, a new annotation PlatformAPI is introduced for marking API status.

--- src/main/java/net/quantrax/messagebuilder/MessageBuilder.java
+++ src/main/java/net/quantrax/messagebuilder/MessageBuilder.java
@@ -1,4 +1,21 @@
 package net.quantrax.messagebuilder;
 
+import net.quantrax.messagebuilder.util.PlatformAPI;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
+
 public interface MessageBuilder {
+
+	static @NotNull MessageBuilder messageBuilder() {
+		return MessageBuilderImpl.Instances.INSTANCE;
+	}
+
+}
+
+interface Provider {
+
+	@ApiStatus.Internal
+	@PlatformAPI
+	@NotNull MessageBuilder messageBuilder();
+
 }
---
+++ src/main/java/net/quantrax/messagebuilder/MessageBuilderImpl.java
@@ -0,0 +1,15 @@
+package net.quantrax.messagebuilder;
+
+import net.quantrax.messagebuilder.util.Services;
+
+import java.util.Optional;
+
+public class MessageBuilderImpl implements MessageBuilder {
+	private static final Optional<Provider> SERVICE = Services.service(Provider.class);
+
+	static final class Instances {
+		static final MessageBuilder INSTANCE = SERVICE
+				.map(Provider::messageBuilder)
+				.orElseGet(MessageBuilderImpl::new);
+	}
+}
