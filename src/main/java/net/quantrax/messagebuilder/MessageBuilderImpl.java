4596763f6b000f52f1646d7e07e96da30b957c14
Merry <56554514+MerryDev@users.noreply.github.com>
2023-12-09 20:44:46 +0100

    Message fetching (#20)
    * Add Message class and improve Language and MessageBuilderImpl classes
    
    Added a new Message class and implemented a new method in the Language class to get the messages from the database asynchronously. The MessageBuilderImpl class was updated to store these locale-specific messages in a Map object for quick and easy access. This refactoring aims to improve performance and simplify the handling of localized messages.
    
    * Refactor MessageBuilderTest and enhance localization functionality (#19)
    
    Refactored MessageBuilderTest, removing redundant server object and using explicit method calls on MessageBuilder. Improved localization by storing localized messages in a Map within the MessageBuilderImpl class. Added two new tests for validating the absence of German messages and presence of English messages. The aim is to enhance performance and provide simpler management of localized messages.

--- src/main/java/net/quantrax/messagebuilder/MessageBuilderImpl.java
+++ src/main/java/net/quantrax/messagebuilder/MessageBuilderImpl.java
@@ -12,22 +12,15 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Optional;
-import java.util.Properties;
+import java.util.*;
 import java.util.logging.Logger;
 
 public class MessageBuilderImpl implements MessageBuilder {
 
 	private static final Logger LOGGER = Logger.getLogger(MessageBuilderImpl.class.getName());
 	private static final Optional<Provider> SERVICE = Services.service(Provider.class);
-
-	@ApiStatus.Internal
 	private HikariDataSource hikariDataSource;
-
-	@Override
-	public @NotNull LanguageStage language(@NotNull Language language) {
-		return new LanguageStage(language);
-	}
+	private final Map<Language, List<Message>> localizedMessages = new HashMap<>();
 
 	@Override
 	public void setup() {
@@ -45,6 +38,7 @@ public class MessageBuilderImpl implements MessageBuilder {
 			);
 
 			this.hikariDataSource = StaticSaduLoader.start(credentials);
+			this.fetchMessages();
 
 		} catch (IOException exception) {
 			LOGGER.severe("Could not load config.properties file. MessageBuilder will not work.");
@@ -57,10 +51,31 @@ public class MessageBuilderImpl implements MessageBuilder {
 	}
 
 	@Override
+	public @NotNull LanguageStage language(@NotNull Language language) {
+		return new LanguageStage(language);
+	}
+
+	@Override
 	public @NotNull LanguageStage localized(@NotNull Player player) {
 		final Language suitableLanguage = Language.fromLocale(player.locale());
 
 		return new LanguageStage(suitableLanguage);
+	}
+
+	@Override
+	public @NotNull Map<Language, List<Message>> localizedMessages() {
+		return this.localizedMessages;
+	}
+
+	private void fetchMessages() {
+		for (Language language : Language.values()) {
+			Language.messages(language)
+					.whenComplete((messages, $) -> this.localizedMessages.putIfAbsent(language, messages))
+					.exceptionally(throwable -> {
+						LOGGER.severe(throwable.getLocalizedMessage());
+						return Collections.emptyList();
+					});
+		}
 	}
 
 	static final class Instances {
